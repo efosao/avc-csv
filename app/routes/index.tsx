1cb4a9bd63222629fc420256338d78c84d8d4fb2
Efosa Oyegun <852788+efosao@users.noreply.github.com>
2022-10-16 13:08:58 -0700

    add core remix files wh layout and index pg

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Link } from "@remix-run/react";
+
+export default function Index() {
+  return (
+    <div className="mx-auto mt-16 max-w-7xl text-center">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
