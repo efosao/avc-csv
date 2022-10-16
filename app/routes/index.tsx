4ffbe6ac250a247f034e5183e073b73cc6dca344
Efosa Oyegun <852788+efosao@users.noreply.github.com>
2022-10-16 15:25:13 -0700

    save unique device-id in localstorage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,21 @@
-import { Link } from "@remix-run/react";
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  const [deviceId, setDeviceId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const savedDeviceId = localStorage.getItem("deviceId");
+    if (savedDeviceId) return setDeviceId(savedDeviceId);
+
+    const newDeviceId = Math.random().toString(36).substring(2, 15);
+    localStorage.setItem("deviceId", newDeviceId);
+    setDeviceId(newDeviceId);
+  }, []);
+
   return (
     <div className="mx-auto mt-16 max-w-7xl text-center">
       <h1>Hello World</h1>
+      <p>Device ID: {deviceId}</p>
     </div>
   );
 }
